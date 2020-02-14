d2790542962912341be2984dfcb1e4ab39e38a76
SimJunSik <wnstlr24@naver.com>
2020-02-14 23:40:24 +0900

    02142337

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ const { getUsers, getUser } = require("../controller");
 
 const router = new Router();
 
-router.get("/", ctx => (ctx.body = "안녕하세요, 심준식입니다."));
+router.get("/", ctx => (ctx.body = "안녕하세요, 심준식입니다.1"));
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUser);
